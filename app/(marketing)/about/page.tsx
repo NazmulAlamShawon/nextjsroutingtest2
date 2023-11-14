de3e2f090da7955ee0c4e652c9dcce76824f554e
NazmulAlamShawon <shawonkst15@gmail.com>
2023-11-14 18:17:45 +0600

    add blog route again

---
+++ app/(marketing)/about/page.tsx
@@ -0,0 +1,7 @@
+export default function Page( ) {
+    return(
+        <div>
+          <h2> test dynamic id aboute route </h2>
+        </div>
+    )
+  }
---
+++ app/(marketing)/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Page({ params }: { params: { categoryId: string, itemId: string} }) {
+    return(
+        <div>
+          <h2> test dynamic blog route </h2>
+        </div>
+    )
+  }
